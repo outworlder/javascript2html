9434cc9b602f1b27cbf4f1cf0fbf710d4adaa6a6
Stephen Eilert <spedrosa@gmail.com>
2008-02-27 04:46:58 +0000

    Initial implementation, need to fix macro.
    git-svn-id: svn+ssh://ssh.phx.nearlyfreespeech.net/home/private/repositories/ufc/compilers/javascript2html@1 962dcc55-0ee8-4b9c-8235-3be0b11de702

---
+++ parser.scm
@@ -0,0 +1,63 @@
+;; Javascript pretty printer (to HTML)
+;; Stephen Pedrosa Eilert, 2008
+;; Construção de Compiladores 2008.1 - Riverson Rios
+
+;; First things first: Parse the command line and open a file
+
+;; Javascript identifiers
+(define javascript-identifiers
+  '(if then else return var))
+
+;; Command line format:
+;; javascript2html <-o output file> input_file
+
+(define input-files
+  '())
+
+(define (process-output-command arguments)
+  (print "Output command, arguments:" arguments))
+
+(define command-line-parameters
+  '(
+    ("-o" process-output-command)
+    ("-css" process-css-command)))
+
+(define (get-command-line-token line)
+  (if (null? line)
+      #f
+      (begin
+        (print "get-command-line-token, line: " (car line))
+        (let ((member-value (assoc (car line) command-line-parameters)))
+          (if member-value
+              (cons 'command (cdr member-value))
+              (cons 'other (car line)))))))
+
+(define (display-usage error)
+  (when error
+    (print "Error: " error)
+    (newline))
+  (print "Usage: javascript2html [-o output_file] input_file")
+  (newline))
+
+(define-macro (call-operation ass-list parameters)
+  (let ((operation (cdr '(ass-list))))
+   `(,@operation ,parameters)))
+
+(define (parse-command-line parameters)
+  (unless (null? parameters)
+    (let ((argument (get-command-line-token parameters)))
+      (print "Argument: " argument)
+      (print "Car: " (car argument))
+      (print "Parameters: " (cdr parameters))
+      (case (car argument)
+        ((command) (call-operation argument (cdr parameters)))
+        ((other) (cons (cdr argument) input-files))))))
+
+(define (open-js-file filename)(print "Load file here."))
+
+(define (main args)
+  (if (null? args)
+      (display-usage #f)
+      (parse-command-line args)))
+
+(main (cdr (argv)))
