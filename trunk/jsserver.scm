b45b9d22efb38dd045d6fb887e4b748ca1eef607
Stephen Eilert <spedrosa@gmail.com>
2008-03-08 20:51:57 +0000

    - Using two cgi applications, one for each page.
    git-svn-id: svn+ssh://ssh.phx.nearlyfreespeech.net/home/private/repositories/ufc/compilers/javascript2html@34 962dcc55-0ee8-4b9c-8235-3be0b11de702

--- trunk/jsserver.scm
+++ trunk/jsserver.scm
@@ -3,3 +3,11 @@
 
 (require 'web-scheme)
 
+(define start-page
+  (ws:page
+   (head "Javascript Pretty Printer"
+         (body (hr) "Entre com o código Javascript Abaixo:"
+               (input 'type "textbox" )
+               (input 'type "submit" 'value "Enviar")))))
+
+       
---
+++ trunk/results-page.scm
@@ -0,0 +1,10 @@
+;; Results page
+;; 
+
+(require 'web-scheme)
+
+(define results-page
+(ws:page
+ (head "Javascript Pretty Printer - Resultados"
+       (body 
+        (link 'value "../start-page")))))
